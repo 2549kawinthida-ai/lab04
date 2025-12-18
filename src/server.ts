0f66a1ad46838e811a51f0c166cadd2bb2f3625f
2549kawinthida-ai <2549kawinthida@gmail.com>
2025-12-18 16:01:20 +0700

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@ app.get("/hello", (req, res) => {
     const name = String(req.query.name ?? "student");
     res.send(`<h1>Hello, ${formatTitle(name)}!</h1>`);
 });
+app.listen(3000, () => {
+console.log("Open: http://localhost:3000/hello?name=toey");
+});
+
 
 //Problem 3
 app.get("/", (req, res) => {
